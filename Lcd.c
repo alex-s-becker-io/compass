a3c742bd7a9a20f5e6e08de051344e55d8707824
Alexandria B <whsitchy@gmail.com>
2012-12-11 21:43:18 -0600

    updating lcd.c

--- Lcd.c
+++ Lcd.c
@@ -48,14 +48,10 @@ void WriteLine(const char* Str, unit8_t Line) {
     uint8_t StartAddr = 0x40 * Line; //DEFINE 0x40
     uint8_t i;
 
-    PORTD &= ~(1 << PD0); /* Make sure RS is set to 0 */
-    PORTB |= SET_CURSOR | StartAddr; /* Go to the start of the Line */
-    delay_us(50);
-
-    PORTD |= (1 << PD0); /* Set RS to 1 */
+    SendByte(SET_CURSOR | StartAddr, TRUE); /* Go to the start of the Line */
 
     for(i = 0; i < strlen(Str); i ++) {
-        PORTB = Str[i];
+        SendByte(Str[i], FALSE);
         delay_us(50);
     }
 }
